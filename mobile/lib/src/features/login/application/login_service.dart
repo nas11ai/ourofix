aeb91b48c1677b74cd4fd524456ae68dfffc7845
nas11ai <74126181+nas11ai@users.noreply.github.com>
2023-09-16 11:27:58 +0800

    fix: update login service url

--- mobile/lib/src/features/login/application/login_service.dart
+++ mobile/lib/src/features/login/application/login_service.dart
@@ -9,10 +9,10 @@ import 'package:mobile/src/features/login/domain/login_response.dart';
 
 part 'login_service.g.dart';
 
-@RestApi(baseUrl: "${API.baseUrl}/users/login")
+@RestApi(baseUrl: API.baseUrl)
 abstract class LoginService {
   factory LoginService(Dio dio, {String baseUrl}) = _LoginService;
 
-  @POST("/login")
+  @POST("/users/login")
   Future<LoginResponse> login(@Body() LoginRequest request);
 }
