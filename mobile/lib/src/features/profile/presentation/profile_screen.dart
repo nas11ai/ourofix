c92b6d6150f7bff23c1502bc5647577621fbcb4e
nas11ai <74126181+nas11ai@users.noreply.github.com>
2023-09-29 04:49:47 +0800

    fix: overflow at profile screen

--- mobile/lib/src/features/profile/presentation/profile_screen.dart
+++ mobile/lib/src/features/profile/presentation/profile_screen.dart
@@ -36,131 +36,133 @@ class ProfileScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: Column(
-          children: [
-            gapH32,
-            Center(
-              child: Stack(
-                children: [
-                  SizedBox(
-                    width: MediaQuery.sizeOf(context).width * 0.3,
-                    height: MediaQuery.sizeOf(context).width * 0.3,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade300,
-                        ),
-                        child: Icon(
-                          Icons.person,
-                          size: MediaQuery.sizeOf(context).width * 0.25,
-                          color: ThemeColor.primaryColor,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    right: 0,
-                    child: Container(
-                      width: MediaQuery.sizeOf(context).width * 0.09,
-                      height: MediaQuery.sizeOf(context).width * 0.09,
-                      decoration: BoxDecoration(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              gapH32,
+              Center(
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.sizeOf(context).width * 0.3,
+                      height: MediaQuery.sizeOf(context).width * 0.3,
+                      child: ClipRRect(
                         borderRadius: BorderRadius.circular(100),
-                        color: Colors.grey.shade600,
-                      ),
-                      child: IconButton(
-                        padding: EdgeInsets.zero,
-                        icon: Icon(
-                          Icons.camera_alt_rounded,
-                          color: ThemeColor.primaryColor,
-                          size: MediaQuery.sizeOf(context).width * 0.06,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade300,
+                          ),
+                          child: Icon(
+                            Icons.person,
+                            size: MediaQuery.sizeOf(context).width * 0.25,
+                            color: ThemeColor.primaryColor,
+                          ),
                         ),
-                        onPressed: () {},
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            gapH64,
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
-                children: [
-                  Text(
-                    'Informasi Akun',
-                    style: Theme.of(context).textTheme.titleMedium!.merge(
-                          const TextStyle(color: Colors.black),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: MediaQuery.sizeOf(context).width * 0.09,
+                        height: MediaQuery.sizeOf(context).width * 0.09,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.grey.shade600,
                         ),
-                  ),
-                  gapH16,
-                  customListTile(
-                    context,
-                    "Username",
-                    "john_doe",
-                    () {
-                      if (context.mounted) {
-                        context.goNamed(AppRoute.editUsername.name);
-                      }
-                    },
-                  ),
-                  gapH16,
-                  customListTile(
-                    context,
-                    "Email",
-                    "john_doe@gmail.com",
-                    () {
-                      if (context.mounted) {
-                        context.goNamed(AppRoute.editEmail.name);
-                      }
-                    },
-                  ),
-                  gapH16,
-                  customListTile(
-                    context,
-                    "Password",
-                    "**********",
-                    () {
-                      if (context.mounted) {
-                        context.goNamed(AppRoute.editPassword.name);
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-            gapH16,
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.only(right: 48.0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    side: const BorderSide(color: ThemeColor.secondaryColor),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0)),
-                  ),
-                  child: SizedBox(
-                    width: MediaQuery.sizeOf(context).width * 0.2,
-                    height: MediaQuery.sizeOf(context).width * 0.1,
-                    child: const Center(
-                      child: Text(
-                        'Logout',
-                        style: TextStyle(color: ThemeColor.primaryColor),
+                        child: IconButton(
+                          padding: EdgeInsets.zero,
+                          icon: Icon(
+                            Icons.camera_alt_rounded,
+                            color: ThemeColor.primaryColor,
+                            size: MediaQuery.sizeOf(context).width * 0.06,
+                          ),
+                          onPressed: () {},
+                        ),
                       ),
                     ),
-                  ),
-                  onPressed: () {
-                    if (context.mounted) {
-                      context.goNamed(AppRoute.login.name);
-                    }
-                  },
+                  ],
                 ),
               ),
-            ),
-          ],
+              gapH64,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Informasi Akun',
+                      style: Theme.of(context).textTheme.titleMedium!.merge(
+                            const TextStyle(color: Colors.black),
+                          ),
+                    ),
+                    gapH16,
+                    customListTile(
+                      context,
+                      "Username",
+                      "john_doe",
+                      () {
+                        if (context.mounted) {
+                          context.goNamed(AppRoute.editUsername.name);
+                        }
+                      },
+                    ),
+                    gapH16,
+                    customListTile(
+                      context,
+                      "Email",
+                      "john_doe@gmail.com",
+                      () {
+                        if (context.mounted) {
+                          context.goNamed(AppRoute.editEmail.name);
+                        }
+                      },
+                    ),
+                    gapH16,
+                    customListTile(
+                      context,
+                      "Password",
+                      "**********",
+                      () {
+                        if (context.mounted) {
+                          context.goNamed(AppRoute.editPassword.name);
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              gapH16,
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 48.0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      side: const BorderSide(color: ThemeColor.secondaryColor),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                    ),
+                    child: SizedBox(
+                      width: MediaQuery.sizeOf(context).width * 0.2,
+                      height: MediaQuery.sizeOf(context).width * 0.1,
+                      child: const Center(
+                        child: Text(
+                          'Logout',
+                          style: TextStyle(color: ThemeColor.primaryColor),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (context.mounted) {
+                        context.goNamed(AppRoute.login.name);
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
