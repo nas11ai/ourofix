1d319f172b5adbf77b4a36ec74598c2255f5c442
nas11ai <74126181+nas11ai@users.noreply.github.com>
2023-11-24 06:59:40 +0800

    refactor: delete unused import

--- mobile/lib/src/features/chat/presentation/chat_detail_screen.dart
+++ mobile/lib/src/features/chat/presentation/chat_detail_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:mobile/src/constants/app_sizes.dart';
 import 'package:mobile/src/constants/theme_colors.dart';
 import 'package:mobile/src/features/chat/data/chat_repository.dart';
 import 'package:mobile/src/features/chat/domain/message.dart';
