43d501f29dc2a1d4529095364fa5e79d7a9ea3d6
nas11ai <kairupannasai@gmail.com>
2023-11-24 15:10:40 +0800

    feat: add transaction data in order

--- backend/src/features/order/models/order.js
+++ backend/src/features/order/models/order.js
@@ -48,6 +48,7 @@ Order.init({
   status: {
     type: DataTypes.ENUM('Sedang dalam antrian', 'Sedang diperbaiki', 'Selesai'),
     allowNull: false,
+    defaultValue: 'Sedang dalam antrian',
     validate: {
       notNull: {
         msg: 'Status perbaikan tidak boleh kosong!',
@@ -55,6 +56,24 @@ Order.init({
       isIn: {
         args: [['Sedang dalam antrian', 'Sedang diperbaiki', 'Selesai']],
         msg: 'Status perbaikan tidak valid!',
+      },
+    },
+  },
+  response_midtrans: {
+    type: DataTypes.TEXT('long'),
+    allowNull: true,
+  },
+  status_transaksi: {
+    type: DataTypes.ENUM('Belum dibayar', 'Diproses', 'Berhasil', 'Gagal'),
+    allowNull: false,
+    defaultValue: 'Belum dibayar',
+    validate: {
+      notNull: {
+        msg: 'Status transaksi tidak boleh kosong!',
+      },
+      isIn: {
+        args: [['Belum dibayar', 'Diproses', 'Berhasil', 'Gagal']],
+        msg: 'Status transaksi tidak valid!',
       },
     },
   },
--- backend/src/migrations/2023_11_24_add_order_status.js
+++ backend/src/migrations/2023_11_24_add_order_status.js
@@ -6,9 +6,38 @@ module.exports = {
       type: DataTypes.ENUM('Sedang dalam antrian', 'Sedang diperbaiki', 'Selesai'),
       allowNull: false,
       defaultValue: 'Sedang dalam antrian',
+      validate: {
+        notNull: {
+          msg: 'Status perbaikan tidak boleh kosong!',
+        },
+        isIn: {
+          args: [['Sedang dalam antrian', 'Sedang diperbaiki', 'Selesai']],
+          msg: 'Status perbaikan tidak valid!',
+        },
+      },
+    });
+    await queryInterface.addColumn('orders', 'response_midtrans', {
+      type: DataTypes.TEXT('long'),
+      allowNull: true,
+    });
+    await queryInterface.addColumn('orders', 'status_transaksi', {
+      type: DataTypes.ENUM('Belum dibayar', 'Diproses', 'Berhasil', 'Gagal'),
+      allowNull: false,
+      defaultValue: 'Belum dibayar',
+      validate: {
+        notNull: {
+          msg: 'Status transaksi tidak boleh kosong!',
+        },
+        isIn: {
+          args: [['Belum dibayar', 'Diproses', 'Berhasil', 'Gagal']],
+          msg: 'Status transaksi tidak valid!',
+        },
+      },
     });
   },
   down: async ({ context: queryInterface }) => {
     await queryInterface.removeColumn('orders', 'status');
+    await queryInterface.removeColumn('orders', 'response_midtrans');
+    await queryInterface.removeColumn('orders', 'status_transaksi');
   },
 };
